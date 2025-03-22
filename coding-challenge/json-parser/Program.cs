f4a551771841ff3baba0e918edaa051866137314
pranavj <pranavj@carestack.com>
2025-03-22 18:48:21 +0530

    coding-challenge-init

---
+++ coding-challenge/json-parser/Program.cs
@@ -0,0 +1,71 @@
+﻿struct Node{
+  public object Val {get; init;}
+  public NodeType Type {get; init;}
+}
+
+class JsonParser{
+  public static void Main(string[] args){
+    string s;
+    using(StreamReader str = File.OpenText("step1/valid.json"))
+    {
+      s = str.ReadToEnd();
+    }
+    Console.WriteLine(s);
+  }
+}
+
+class Lexer{
+  string s = "";
+  int index = 0;
+  public Lexer(string s){
+    this.s = s;
+  }
+  public Node GetNext(){
+    if(s.Length == index){
+      return new Node{
+        Val = "",
+        Type = NodeType.End
+      };
+    }
+
+    if(s[index] == '"'){
+      return new Node{
+        Val = "\"",
+        Type = NodeType.DoubleQuotes
+      };
+    }
+    else if(s[index] == '\''){
+      return new Node{
+        Val = "'",
+        Type = NodeType.SingleQuotes
+      };
+    }
+    else if(char.IsAsciiLetterOrDigit(s[index])){
+      string val = "";
+      while(index < s.Length && char.IsAsciiLetterOrDigit(s[index])){
+        val += s[index];
+        index++;
+      }
+      return new Node{
+        Val = val,
+        Type = NodeType.String
+      };
+    }
+    return new Node{};
+  }
+}
+
+
+enum NodeType{
+  DoubleQuotes,
+  SingleQuotes,
+  String,
+  Colon,
+  Number,
+  Float,
+  SquareOpen,
+  SquareClose,
+  CurlyOpen,
+  CurlyClose,
+  End,
+}
---
+++ coding-challenge/wc/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.RegularExpressions;
+
+class WC
+{
+  public static void Main(string []args)
+  {
+    string s;
+    using(StreamReader sr = File.OpenText("test.txt"))
+    {
+       s = sr.ReadToEnd();
+    }
+    string val = Console.ReadLine();
+    val = val.Trim();
+    var input = val.Split(" ");
+    List<string> validArgs = ["-c","-l", "-w", "-m"];
+
+    if(input[0] != "wc")
+    {
+      Console.WriteLine($"Unkown command {input[0]}");
+    }
+    else if (input.Length > 1 && !validArgs.Contains(input[1]))
+    {
+      Console.WriteLine($"Invalid arg {input[1]}");
+    }
+    else if(input.Length > 2)
+    {
+      Console.WriteLine("Invalid format for the command");
+    }
+
+    if(input.Length == 1 || input[1] == "-c")
+      Console.WriteLine(s.Length);
+    if(input.Length == 1 || input[1] == "-l")
+      Console.WriteLine(s.Count(c => c == '\n' ));
+    if(input.Length == 1 || input[1] == "-w")
+    {
+      var words = s.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries).Length;
+      Console.WriteLine(words);
+    }
+    if(input.Length == 1 || input[1] == "-m"){
+      Console.WriteLine(s.Length);
+    }
+  }
+}
